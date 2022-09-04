ea1d37b2a1fd7572f060fdbd31de0188921c9a4f
isaac <isaacd4444@gmail.com>
2022-09-04 13:23:37 -0400

    built usesignup hooks and connected to firebase

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import { projectAuth } from "../firebase/config"
+export const useSignup=()=>{
+    const [error,setError]=useState(null);
+    const [isPending, setIsPending]=useState(false);
+    const signup=async(email, password,userName)=>{
+        setError(null);
+        setIsPending(true);
+        try{
+            //sign up user
+            const response=await projectAuth.createUserWithEmailAndPassword(email,password)
+            console.log(response.user)
+            if(!response){
+                throw new Error("Could not complete signup")
+            }
+            //add display name to user
+            await response.user.updateProfile({displayName:userName})
+            setIsPending(false);
+            setError(null);
+
+        }catch(err){
+            console.log(err.message);
+            setError(err.message);
+            setIsPending(false);
+        }
+
+    }
+    return {error,isPending,signup}
+
+
+}
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,15 +1,16 @@
 import { useState } from 'react'
+import {useSignup} from '../../hooks/useSignup'
 import styles from './Signup.module.css'
 
 
 export default function Signup() {
-  
+  const {error, isPending,signup}=useSignup();
   const [username,setUsername]=useState('');
   const [password,setPassword]=useState('');
   const [email,setEmail]=useState('');
   const handleSubmit=(e)=>{
     e.preventDefault();
-    console.log(username,email,password)
+    signup(email,password,username);
   }
   return (
     <form className={styles['signup-form']} onSubmit={handleSubmit}>
@@ -35,7 +36,10 @@ export default function Signup() {
         value={password}
         />
       </label>
-      <button type='submit' className='btn'>Sign Up</button>
+      {!isPending && <button type='submit' className='btn'>Sign Up</button>}
+      {isPending && <button className='btn' disabled>Loading</button>}
+      {error && <p>{error}</p>}
+      
     </form>
   )
 }
