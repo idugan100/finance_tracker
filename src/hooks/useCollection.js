7d4e13b5558a81689d302bc321b9249cd907137d
isaac <isaacd4444@gmail.com>
2022-09-04 23:00:55 -0400

    implemented firebase query for users transactions

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,19 @@
 import { projectFirestore } from "../firebase/config"
-import { useEffect,useState } from "react"
+import { useEffect,useRef,useState } from "react"
 
-export const useCollection=(collection)=>{
+
+export const useCollection=(collection,_query)=>{
+    
     const [documents,setDocuments]=useState(null);
     const [error,setError]=useState(null);
+
+    const query=useRef(_query).current
      
     useEffect(()=>{
         let ref=projectFirestore.collection(collection);
+        if(query){
+            ref=ref.where(...query)
+        }
         const usub=ref.onSnapshot((snapshot)=>{ 
             let results=[];
             snapshot.docs.forEach((doc=>{results.push({...doc.data(),id:doc.id})
@@ -16,7 +23,7 @@ export const useCollection=(collection)=>{
             setError(null);
         },(error)=>{setError(error.message)})
         return ()=>usub()
-    },[collection])
+    },[collection,query])
 
     return {documents,error}
 
