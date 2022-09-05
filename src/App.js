a0a696c3839a0728a8672e942f168726dbf37a00
isaac <isaacd4444@gmail.com>
2022-09-04 20:55:29 -0400

    created transaction form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter,Switch,Route } from 'react-router-dom'; 
+import { BrowserRouter,Switch,Route,Redirect} from 'react-router-dom'; 
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup'
 import Home from './pages/home/Home';
@@ -8,7 +8,7 @@ import {useAuthContext} from './hooks/useAuthContext'
 
 
 function App() {
-  const {authIsReady}=useAuthContext();
+  const {authIsReady, user}=useAuthContext();
   return (
     <div className="App">
       {authIsReady &&(
@@ -16,15 +16,17 @@ function App() {
       <Navbar/>
       <Switch>
         <Route exact path='/login'>
-         
-          <Login/>
+          {user && <Redirect to='/'/>}
+          {!user &&<Login/>}
         </Route>
         <Route path='/signup'>
-          <Signup/>
+          {user && <Redirect to='/'/>}
+          {!user && <Signup/>}
 
         </Route>
         <Route exact path='/'> 
-          <Home/>
+          {!user && <Redirect to='/login'/>}
+          {user && <Home/>}
           </Route>
       </Switch>
        
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,9 @@ import styles from './Home.module.css'
 export default function Home() {
   
   return (
-    <div>Home:</div>
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar }> transction fomr</div>
+    </div>
   )
 }
